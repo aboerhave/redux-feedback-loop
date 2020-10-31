9d7cdfd7eaef2bd40c7b57c1499f231640db92a3
Adam Boerhave <arboerhave@gmail.com>
2020-10-30 20:21:04 -0500

    elements for feeling exist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import {connect} from 'react-redux';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import Header from '../Header/Header';
+import Feeling from '../Feeling/Feeling';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <br/>
-      </div>
+      return (
+        <Router>
+        <div className="App">
+          <Header />
+
+          <Route exact path='/' component={Feeling} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Feeling/Feeling.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {connect} from 'react-redux';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import Header from '../Header/Header';
+
+class Feeling extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h2>How are you feeling today? (1-10)</h2>
+          <label>Feeling?</label>
+          <br/>
+          <select name="cars" id="cars">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+      </div>
+    );
+  }
+}
+
+const putReduxStateOnProps = (reduxStore) => ({
+  reduxStore
+})
+
+export default connect(putReduxStateOnProps)(Feeling);
