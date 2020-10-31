32a11d9b2d4e831bbf656522f6512a6185b4fe10
Adam Boerhave <arboerhave@gmail.com>
2020-10-30 19:58:31 -0500

    header componentized

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,13 @@ import axios from 'axios';
 import './App.css';
 import {connect} from 'react-redux';
 import {HashRouter as Router, Route} from 'react-router-dom';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+        <Header />
         <br/>
       </div>
     );
